70fa4e56de7cc8e764ead7f5331b1fd5ceb84b86
Rafael Felipe <rafael.felipe1989@gmail.com>
2020-10-31 17:23:56 -0300

    create sample app

---
+++ Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by rafael ramos on 31/10/20.
+//
+
+import UIKit
+import GSKit
+
+class ViewController: GSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        changeBackground(withColor: .red)
+        
+    }
+
+
+}
+
---
+++ GSKit/GSViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GSViewController.swift
+//  GSKit
+//
+//  Created by rafael ramos on 31/10/20.
+//
+
+import UIKit
+
+open class GSViewController: UIViewController {
+    public func changeBackground(withColor color: UIColor) {
+        view.backgroundColor = color
+    }
+    
+}
